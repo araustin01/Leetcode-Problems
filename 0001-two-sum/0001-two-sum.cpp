a047e253605211bbfdddeab31c59d7ca1be45c2c
Alex Austin <72994707+araustin01@users.noreply.github.com>
2024-03-12 16:41:17 -0400

    Time: 130 ms (17.05%), Space: 12.6 MB (64.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,9 +4,7 @@ public:
         int length = nums.size();
         vector<int> ans;
         for (int i = 0; i < length; i++) {
-            for (int j = length - 1; j >= 0; j--) {
-                if(i == j)
-                    break;
+            for (int j = length - 1; j > i; j--) {
                 if(nums[i] + nums[j] == target) {
                     ans.push_back(i);
                     ans.push_back(j);
