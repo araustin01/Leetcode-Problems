e86c25b09bd92b638b0907faf122f6b3fde4cc8e
Alex Austin <72994707+araustin01@users.noreply.github.com>
2024-03-12 16:40:59 -0400

    Time: 110 ms (32.60%), Space: 6.3 MB (32.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -4,9 +4,7 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int* ans = malloc(2 * sizeof(int));
     for (int i = 0; i < numsSize; i++) {
-        for (int j = numsSize - 1; j >= 0; j--) {
-            if(i == j)
-                break;
+        for (int j = numsSize - 1; j > i; j--) {
             if(nums[i] + nums[j] == target) {
                 ans[0] = i;
                 ans[1] = j;
