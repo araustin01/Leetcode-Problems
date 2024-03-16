aad3f76de16fa1e5560e4ee39e1105648355bdac
Alex Austin <72994707+araustin01@users.noreply.github.com>
2024-03-16 00:32:11 -0400

    Time: 83 ms (75.04%), Space: 57 MB (49.45%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.ts
@@ -3,12 +3,11 @@ function lengthOfLongestSubstring(s: string): number {
     var lsub: string = sub; // largest sub
     
     for(let i = 0; i < s.length; i++) {
-        console.log(`sub=${sub} // lsub=${lsub}`);
         let c: string = s.charAt(i);
         let index: number = c.charCodeAt(0) - 'a'.charCodeAt(0); // character encoding (a-z => 0-25)
-        sub = sub.substring(sub.indexOf(c)+1) + c
+        sub = sub.substring(sub.indexOf(c)+1) + c // shift string appropriately 
         if(sub.length > lsub.length)
-            lsub = sub;
+            lsub = sub; // track largest consecutive string
     }
     return lsub.length;
 };
