c869f453e7b04e34ec13d2f5677fcd9ad51623d5
Alex Austin <72994707+araustin01@users.noreply.github.com>
2024-03-20 17:01:51 -0400

    Time: 17 ms (75.27%), Space: 42.1 MB (86.24%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,31 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.isEmpty()) 
+        if (s == null || s.isEmpty())
             return "";
 
-        int start = 0; 
-        int maxLength = 1; 
+        int start = 0;
+        int maxLength = 1;
         int n = s.length();
 
-        for (int i = 0; i < n; i++) {
-            int left = i;
-            int right = i;
+        int left; int right;
 
-            // Expand around center for odd-length palindromes (aba)
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+        for (int i = 0; i < n; i++) {
+            // Expand around center for odd-length palindromes (e.g. aba)
+            for (left = i, right = i; left >= 0 && right < n && s.charAt(left) == s.charAt(right); left--, right++) {
                 int length = right - left + 1;
                 if (length > maxLength) { // is next longest palindrome
                     maxLength = length;
                     start = left;
                 }
-                left--;
-                right++;
             }
 
-            left = i;
-            right = i + 1;
-
-            // Expand around center for even-length palindromes (abba)
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+            // Expand around center for even-length palindromes (e.g. abba)
+            for (left = i, right = i+1; left >= 0 && right < n && s.charAt(left) == s.charAt(right); left--, right++) {
                 int length = right - left + 1;
                 if (length > maxLength) { // is next longest palindrome
                     maxLength = length;
                     start = left;
                 }
-                left--;
-                right++;
             }
         }
 
